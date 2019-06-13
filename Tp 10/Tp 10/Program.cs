0e0221ab5612664660f119b3b8f22cdfb6b1c46f
Arzenus <nicolastarulli@hotmail.com>
2019-06-13 18:38:23 -0300

    Metodos de la clase agregados

---
+++ Tp 10/Tp 10/Program.cs
@@ -0,0 +1,34 @@
+﻿using Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp_10
+{
+    enum TipoDeOperacion
+    {
+        Venta,
+        Alquiler
+    }
+
+    enum TipoDePropiedad
+    {
+        Departamento,
+        Casa,
+        Duplex,
+        Penthouse,
+        Terreno
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Propiedad prop1 = new Propiedad();
+            prop1.TipoDePropiedad = "casa";
+        }
+    }
+
+}
---
+++ Tp 10/Tp 10/Propiedad.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    class Propiedad
+    {
+        private int id;
+        private string tipoDePropiedad;
+        private string tipoDeOperacion;
+        private float tamanio;
+        private int cantidadDeBanios;
+        private int cantidadDeHabitaciones;
+        private string domicilio;
+        private double precio;
+        private bool estado;
+        private double valorDelInmueble;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string TipoDePropiedad
+        {
+            get
+            {
+                return tipoDePropiedad;
+            }
+
+            set
+            {
+                tipoDePropiedad = value;
+            }
+        }
+
+        public string TipoDeOperacion
+        {
+            get
+            {
+                return tipoDeOperacion;
+            }
+
+            set
+            {
+                tipoDeOperacion = value;
+            }
+        }
+
+        public float Tamanio
+        {
+            get
+            {
+                return tamanio;
+            }
+
+            set
+            {
+                tamanio = value;
+            }
+        }
+
+        public int CantidadDeBanios
+        {
+            get
+            {
+                return cantidadDeBanios;
+            }
+
+            set
+            {
+                cantidadDeBanios = value;
+            }
+        }
+
+        public int CantidadDeHabitaciones
+        {
+            get
+            {
+                return cantidadDeHabitaciones;
+            }
+
+            set
+            {
+                cantidadDeHabitaciones = value;
+            }
+        }
+
+        public string Domicilio
+        {
+            get
+            {
+                return domicilio;
+            }
+
+            set
+            {
+                domicilio = value;
+            }
+        }
+
+        public double Precio
+        {
+            get
+            {
+                return precio;
+            }
+
+            set
+            {
+                precio = value;
+            }
+        }
+
+        public bool Estado
+        {
+            get
+            {
+                return estado;
+            }
+
+            set
+            {
+                estado = value;
+            }
+        }
+    }
+}
